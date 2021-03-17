d410ba01e8033877a72615e2185d7a3170e0ea3a
ray <rayphelan@gmail.com>
2021-03-17 14:26:46 +0000

    First commit - Basic functionality with Fibonacci to WASM in C, C++ and Zig

---
+++ Algorithms/Zig/Fibonacci/fibonacci.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const source = fs.readFileSync("./fibonacci.wasm");
+const typedArray = new Uint8Array(source);
+
+WebAssembly.instantiate(typedArray, {
+    env: {
+        print: (result) => { console.log(result); }
+    }
+}).then(result => {
+    const fib = result.instance.exports.fib;
+    fib(20);
+});
