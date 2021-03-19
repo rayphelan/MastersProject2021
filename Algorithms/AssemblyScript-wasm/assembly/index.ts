91d603344643e9ef04db3f4fac5bf96ad05acc60
ray <rayphelan@gmail.com>
2021-03-19 19:54:22 +0000

    Removed AssemblyScript

--- Algorithms/AssemblyScript-wasm/assembly/index.ts
+++
@@ -1,9 +0,0 @@
-// The entry file of your WebAssembly module.
-
-export function add(a: i32, b: i32): i32 {
-  return a + b;
-}
-
-export function fibonacci(a: i32): i32 {
-  return a <= 1 ? 1 : fibonacci(a - 1) + fibonacci(a -2);
-}
