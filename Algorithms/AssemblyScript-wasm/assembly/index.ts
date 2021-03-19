4c9bd60d90470bc306166535766d111fdb850a8f
ray <rayphelan@gmail.com>
2021-03-19 19:34:17 +0000

    Removed Javascript

--- Algorithms/AssemblyScript-wasm/assembly/index.ts
+++ Algorithms/AssemblyScript-wasm/assembly/index.ts
@@ -3,3 +3,7 @@
 export function add(a: i32, b: i32): i32 {
   return a + b;
 }
+
+export function fibonacci(a: i32): i32 {
+  return a <= 1 ? 1 : fibonacci(a - 1) + fibonacci(a -2);
+}
--- Algorithms/Javascript/Fibonacci/fibonacci.js
+++
@@ -1,2 +0,0 @@
-const fib = n => n <= 1 ? 1 : fib(n - 1) + fib(n - 2);
-console.log(fib(40));
