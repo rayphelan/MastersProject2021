068a890ac71692cbb8fcc165d144f6b02a2b4ee3
ray <rayphelan@gmail.com>
2021-03-22 19:55:34 +0000

    Added execution timer to AssemblyScript Fibonacci

--- Algorithms/AssemblyScript/Fibonacci/experiment/index.js
+++ Algorithms/AssemblyScript/Fibonacci/experiment/index.js
@@ -1,3 +1,12 @@
 const myModule = require("..");
+
+// Begin timer
+const start = process.hrtime();
+
 const result = myModule.fibonacci(40);
+
+// End timer
+const diff = process.hrtime(start);
+console.log(`Execution time: ${diff[0] * 1e9 + diff[1]} nanoseconds`);
+
 console.log(result);
