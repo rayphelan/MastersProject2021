c089c0fabcefd2c5f0119cc0ad0cde41f6389a84
ray <rayphelan@gmail.com>
2021-03-27 22:34:05 +0000

    Added PrimeNumber in C

---
+++ Algorithms/C/PrimeNumber/primeNumber.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <math.h>
+int primeNumber(int rangenumber)
+{
+    int c = 0, num = 2, i, letest = 0;
+
+    while (c != rangenumber)
+    {
+        int count = 0;
+        for (i = 2; i <= sqrt(num); i++)
+        {
+            if (num % i == 0)
+            {
+                count++;
+                break;
+            }
+        }
+        if (count == 0)
+        {
+            c++;
+            letest = num;
+        }
+        num = num + 1;
+    }
+    printf("%dth prime number is %d \n", rangenumber, letest);
+    return 0;
+}
