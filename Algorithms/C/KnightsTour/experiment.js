34cd237b312f90be0082cc94e117979c445abbb3
ray <rayphelan@gmail.com>
2021-03-28 19:44:19 +0100

    Added KnightsTour in C

---
+++ Algorithms/C/KnightsTour/experiment.js
@@ -0,0 +1,15 @@
+const wasmModule = require('./knightsTour.js');
+
+// const arraySize = 1000000;
+
+// Start Timer
+const start = process.hrtime();
+
+wasmModule().then((instance) => {
+
+    instance._knightsTour(8);
+
+    // End Timer
+    const diff = process.hrtime(start);
+    console.log(`Execution time: ${diff[0] * 1e9 + diff[1]} nanoseconds`);
+});
