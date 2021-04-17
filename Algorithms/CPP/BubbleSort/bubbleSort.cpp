1b8d9f794428f9d013c07bc37e6820b32de547ab
ray <rayphelan@gmail.com>
2021-04-17 14:11:41 +0100

    Added BubbleSort C++

---
+++ Algorithms/CPP/BubbleSort/bubbleSort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdlib>
+
+extern "C" {
+    int bubbleSort(int arraySize)
+    {
+
+        int temp, array[arraySize];
+
+        srand(0);
+
+        for (int i = 0; i < arraySize; i++) {
+            array[i] = rand();
+        }
+
+        for (int i = arraySize - 2; i >= 0; i--)
+        {
+            for (int j = 0; j <= i; j++)
+            {
+                if (array[j] > array[j + 1])
+                {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < arraySize; i++)
+            printf(" %d \n", array[i]);
+
+        return 0;
+    }
+}
