f5003c0e88f1dece6695ebeec882a87b892d32d4
ray <rayphelan@gmail.com>
2021-04-19 11:18:39 +0100

    Added missing pkg files

---
+++ Algorithms/Rust/bubble-sort/pkg/bubble_sort.js
@@ -0,0 +1,44 @@
+let imports = {};
+imports['__wbindgen_placeholder__'] = module.exports;
+let wasm;
+const { TextDecoder } = require(String.raw`util`);
+
+let cachedTextDecoder = new TextDecoder('utf-8', { ignoreBOM: true, fatal: true });
+
+cachedTextDecoder.decode();
+
+let cachegetUint8Memory0 = null;
+function getUint8Memory0() {
+    if (cachegetUint8Memory0 === null || cachegetUint8Memory0.buffer !== wasm.memory.buffer) {
+        cachegetUint8Memory0 = new Uint8Array(wasm.memory.buffer);
+    }
+    return cachegetUint8Memory0;
+}
+
+function getStringFromWasm0(ptr, len) {
+    return cachedTextDecoder.decode(getUint8Memory0().subarray(ptr, ptr + len));
+}
+/**
+*/
+module.exports.main_js = function() {
+    wasm.main_js();
+};
+
+module.exports.__wbg_log_ddfaf137067decc6 = function(arg0, arg1) {
+    try {
+        console.log(getStringFromWasm0(arg0, arg1));
+    } finally {
+        wasm.__wbindgen_free(arg0, arg1);
+    }
+};
+
+const path = require('path').join(__dirname, 'bubble_sort_bg.wasm');
+const bytes = require('fs').readFileSync(path);
+
+const wasmModule = new WebAssembly.Module(bytes);
+const wasmInstance = new WebAssembly.Instance(wasmModule, imports);
+wasm = wasmInstance.exports;
+module.exports.__wasm = wasm;
+
+wasm.__wbindgen_start();
+
---
+++ Algorithms/Rust/heap-sort/pkg/README.md
@@ -0,0 +1,2 @@
+### Source
+> https://chercher.tech/rust/heap-sort-rust
