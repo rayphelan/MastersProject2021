b8b6bed5cc4ae293481177d4c18a5fd3586bc4b1
ray <rayphelan@gmail.com>
2021-05-14 11:38:28 +0100

    Updated Rust BubbleSort 100000 iterations

--- Algorithms/Rust/bubble-sort/README.md
+++ Algorithms/Rust/bubble-sort/README.md
@@ -1,2 +1,3 @@
 ### Source
 > https://chercher.tech/rust/bubble-sort-rust
+> https://www.hackertouch.com/bubble-sort-in-rust.html
--- Algorithms/Rust/bubble-sort/pkg/bubble_sort.js
+++ Algorithms/Rust/bubble-sort/pkg/bubble_sort.js
@@ -1,35 +1,10 @@
 let imports = {};
-imports['__wbindgen_placeholder__'] = module.exports;
 let wasm;
-const { TextDecoder } = require(String.raw`util`);
 
-let cachedTextDecoder = new TextDecoder('utf-8', { ignoreBOM: true, fatal: true });
-
-cachedTextDecoder.decode();
-
-let cachegetUint8Memory0 = null;
-function getUint8Memory0() {
-    if (cachegetUint8Memory0 === null || cachegetUint8Memory0.buffer !== wasm.memory.buffer) {
-        cachegetUint8Memory0 = new Uint8Array(wasm.memory.buffer);
-    }
-    return cachegetUint8Memory0;
-}
-
-function getStringFromWasm0(ptr, len) {
-    return cachedTextDecoder.decode(getUint8Memory0().subarray(ptr, ptr + len));
-}
 /**
 */
 module.exports.main_js = function() {
     wasm.main_js();
-};
-
-module.exports.__wbg_log_ddfaf137067decc6 = function(arg0, arg1) {
-    try {
-        console.log(getStringFromWasm0(arg0, arg1));
-    } finally {
-        wasm.__wbindgen_free(arg0, arg1);
-    }
 };
 
 const path = require('path').join(__dirname, 'bubble_sort_bg.wasm');
@@ -40,5 +15,14 @@ const wasmInstance = new WebAssembly.Instance(wasmModule, imports);
 wasm = wasmInstance.exports;
 module.exports.__wasm = wasm;
 
+// Start Timer
+const start = process.hrtime();
+
+// Run WASM
 wasm.__wbindgen_start();
+
+// End Timer
+const diff = process.hrtime(start);
+console.log(`Execution time: ${diff[0] * 1e9 + diff[1]} nanoseconds`);
+console.log("Execution time (hr): %ds %dms", diff[0], diff[1]/1000000);
 
