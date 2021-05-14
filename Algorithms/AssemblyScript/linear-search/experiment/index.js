8d52c1a9b1a5b425095b90eb02d173a0b277d473
ray <rayphelan@gmail.com>
2021-05-14 07:35:30 +0100

    Updated AssemblyScript LinearSearch 100000 iterations

--- Algorithms/AssemblyScript/linear-search/experiment/index.js
+++ Algorithms/AssemblyScript/linear-search/experiment/index.js
@@ -10,4 +10,4 @@ const diff = process.hrtime(start);
 console.log(`Execution time: ${diff[0] * 1e9 + diff[1]} nanoseconds`);
 console.log("Execution time (hr): %ds %dms", diff[0], diff[1]/1000000);
 
-console.log(result);
+// console.log(result);
