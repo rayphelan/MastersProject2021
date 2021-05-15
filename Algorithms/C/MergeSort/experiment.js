ec0f80b127c1d65868f12667035bcf73e0a08e1f
ray <rayphelan@gmail.com>
2021-05-15 12:51:03 +0100

    Update C/C++ MergeSort 100000 iterations

--- Algorithms/C/MergeSort/experiment.js
+++ Algorithms/C/MergeSort/experiment.js
@@ -1,15 +1,14 @@
 const wasmModule = require('./mergeSort.js');
 
-const arraySize = 10;
-
 // Start Timer
 const start = process.hrtime();
 
 wasmModule().then((instance) => {
 
-    instance._mergeSort(arraySize);
+    instance._mergeSort();
 
     // End Timer
     const diff = process.hrtime(start);
     console.log(`Execution time: ${diff[0] * 1e9 + diff[1]} nanoseconds`);
+    console.log("Execution time (hr): %ds %dms", diff[0], diff[1]/1000000);
 });
